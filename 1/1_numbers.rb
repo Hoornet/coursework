67318b0fa4835f0d13afa4b2eb3d31346833c1cf
Jure Srsen <Jure Srsen>
2012-10-29 14:28:47 +0100

    start of 2 dirs

---
+++ 1/1_numbers.rb
@@ -0,0 +1,11 @@
+=begin
+   Write a program which tells you:
+   how many hours are in a year?
+   how many minutes are in a decade?
+   how many seconds old are you?
+   how many chocolates do you hope to eat in your life?
+   Warning: This part of the program could take a while to compute!
+   Here's a tougher question:
+If I am 1031 million seconds old, how old am I?
+=end
+
---
+++ 4/4_Mixind_it_up.rb
@@ -0,0 +1,7 @@
+=begin
+• Write a program which asks for a person's first name, then middle, then last. Finally, it should greet the person
+using their full name.
+• Write a program which asks for a person's favorite number. Have your program add one to the number, then suggest
+the result as a bigger and better favorite number. (Do be tactful about it, though.)
+=end
+
